fa74372123f2a3641e245e78a6cc021460602a24
ascelin <ascelin@gmail.com>
2017-07-29 19:10:15 +1000

    Major changes - added a option to specify the number of scenarios to plot (speed things up considerably for testing) Minor changes - change first line to rm(list=ls(all=TRUE)) - minor bug fix in collate_routines.R - don't need any of the libraries to do plotting so have removed them - current_filenames was not used, so removed it and associated references - fixed check to see if any files were found, code now stops if no files found - removed variable data_type as it wasn't used - added comments

--- plot_collated_realisations.R
+++ plot_collated_realisations.R
@@ -1,31 +1,46 @@
-rm(list = ls())
-library(foreach)
-library(doParallel)
-library(abind)
-library(pixmap)
 
-plot_type = 'outcomes' #'impacts', 'outcomes'.
+# Plot the collated simulaton results
+# To run: source('plot_collated_realisations.R')
+
+rm(list=ls(all=TRUE))
+
+    #---------------------
+    # User parameters
+    #---------------------
+
+plot_type = 'impacts' # can be 'outcomes'  or 'impacts',
+num_realisations_to_plot  = -50 # Note a negative number means plot all available
 offset_bank = FALSE
 write_pdf = TRUE
-data_type = 'simulated'
-run_number = 40
-runstring = formatC(run_number, width = 5, format = "d", flag = "0")
+run_number = 50   # for output plot name
 
-collated_folder = paste0('~/offset_data/collated_realisations/')  #LOCATION OF COLLATED FILES
+#collated_folder = paste0('~/offset_data/collated_realisations/')  #LOCATION OF COLLATED FILES
+collated_folder = '/Users/ascelin/analysis/src/offset_simulator/data3/collated_realisations/'
 output_plot_folder = collated_folder
+
+    #---------------------
+    # User parameters
+    #---------------------
+
 
 source('plot_routines.R')                                   # functions to plot collated outputs
 source('collate_routines.R')
 
-run_params = readRDS(paste0(collated_folder, 'run_params.rds'))
-current_filenames <- list.files(path = collated_folder, pattern = '_collated_realisation', all.files = FALSE, 
-                                full.names = FALSE, recursive = FALSE, ignore.case = FALSE, 
-                                include.dirs = FALSE, no.. = FALSE)
+check_plot_options()
+
+runstring = formatC(run_number, width = 5, format = "d", flag = "0")
+run_params = readRDS(paste0(collated_folder, '/run_params.rds'))
+# current_filenames <- list.files(path = collated_folder, pattern = '_collated_realisation', all.files = FALSE, 
+#                                 full.names = FALSE, recursive = FALSE, ignore.case = FALSE, 
+#                                 include.dirs = FALSE, no.. = FALSE)
+
 
 if (!file.exists(output_plot_folder)){
   dir.create(output_plot_folder)
 }
 
+
+# Set the output filename, and open the pdf file for reading
 if (write_pdf == TRUE){
   if (plot_type == 'impacts'){
     filename = paste0(output_plot_folder, runstring, '_impacts.pdf')
@@ -40,7 +55,7 @@ if (offset_bank == TRUE){
   site_impact_plot_lims = c(-1e6, 1e6)
 } else {
   site_plot_lims = c(0, 1e4)
-  site_impact_plot_lims = c(-1e4, 1e4)
+  site_impact_plot_lims = c(-1e4, 2e4)
 }
 
 program_plot_lims = c(0e6, 5e6) 
@@ -51,20 +66,30 @@ landscape_impact_plot_lims = c(-6e5, 6e5)
 sets_to_plot = 5
 setup_sub_plots(nx = 3, ny = 4, x_space = 5, y_space = 5)
 
-policy_num = length(current_filenames)
-
-if (length(current_filenames) == 0){
-  print('no files found')
-}
-
 scenario_filenames <- list.files(path = collated_folder, pattern = '_policy_params', all.files = FALSE, 
                                  full.names = FALSE, recursive = FALSE, ignore.case = FALSE, 
                                  include.dirs = FALSE, no.. = FALSE)
+
+if (length(scenario_filenames) == 0){
+  stop( paste('\nERROR: No files that match _policy_params found in', collated_folder) )
+}
+
+
+
 for (scenario_ind in seq_along(scenario_filenames)){
   current_policy_params = readRDS(paste0(collated_folder, scenario_filenames[scenario_ind]))
   collated_realisations = bind_collated_realisations(scenario_ind, 
                                                      file_path = collated_folder, 
-                                                     eco_ind = 1)
+                                                     eco_ind = 1, num_realisations_to_plot)
+
+  # Check if only plotting a subset of the realisations
+  # Note: a negative number means bind all
+  if( num_realisations_to_plot < 0 ) realisations_to_plot <- run_params$realisation_num
+  else realisations_to_plot <- num_realisations_to_plot
+
+  # In case more realisation are specified then are available
+  if(num_realisations_to_plot > run_params$realisation_num) realisations_to_plot <- run_params$realisation_num
+
   if (plot_type == 'impacts'){
     plot_impact_set(collated_realisations, 
                     current_policy_params, 
@@ -75,7 +100,7 @@ for (scenario_ind in seq_along(scenario_filenames)){
                     lwd_vec = c(3, 0.5), 
                     time_steps = run_params$time_steps, 
                     parcel_num = 1600,
-                    realisation_num = run_params$realisation_num) 
+                    realisation_num = realisations_to_plot) 
   } else {
     plot_outcome_set(collated_realisations,
                      current_policy_params,
@@ -85,12 +110,12 @@ for (scenario_ind in seq_along(scenario_filenames)){
                      sets_to_plot,
                      lwd_vec = c(3, 0.5), 
                      time_steps = run_params$time_steps,
-                     realisation_num = run_params$realisation_num) 
+                     realisation_num = realisations_to_plot) 
   }
   print(paste0('policy ', scenario_ind, ' done'))
 }
 
 
-if (write_pdf == TRUE){
-  dev.off()
-}
+# Close the pdf file for reading
+if (write_pdf == TRUE) dev.off()
+
