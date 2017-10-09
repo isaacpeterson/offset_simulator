fdf79066482c1adb501d3619d120d2a1dad318c3
isaacpeterson <isaac.p.mail@gmail.com>
2017-10-09 10:56:15 +0300

    add functionality to include/exclude offset/dev impacts/outcomes

--- plot_collated_realisations.R
+++ plot_collated_realisations.R
@@ -5,11 +5,18 @@ source('collate_routines.R')
 # User parameters
 #---------------------
 
-plot_type = 'impacts' # can be 'outcomes'  or 'impacts',
+plot_type = 'outcomes' # can be 'outcomes'  or 'impacts',
 output_type = 'features' # set to 'features' for multiple feature layers or 'scenarios' for multiple scenarios
 realisation_num = 'all' # 'all' or number to plot
 offset_bank = FALSE
 write_pdf = FALSE
+plot_site_offset_impact = TRUE 
+plot_site_dev_impact = TRUE
+plot_site_net_impact = FALSE
+
+plot_site_offset_outcome = TRUE
+plot_site_dev_outcome = FALSE
+
 run_number = 4 # for output plot name
 example_set_to_plot = 3 # example site to plot
 plot_vec = 1:2 #c(1,4,7,10, 8, 2,3,5,6,9,11,12 ) #1:12
@@ -115,9 +122,9 @@ for (plot_ind in plot_vec){
   
   if (plot_type == 'impacts'){
     plot_impact_set(collated_realisations, 
-                    plot_site_offset_impact = TRUE, 
-                    plot_site_dev_impact = FALSE, 
-                    plot_site_net_impact = FALSE, 
+                    plot_site_offset_impact, 
+                    plot_site_dev_impact, 
+                    plot_site_net_impact, 
                     output_type,
                     current_policy_params, 
                     site_impact_plot_lims_set[[plot_ind]],
@@ -131,8 +138,8 @@ for (plot_ind in plot_vec){
                     feature_ind = run_params$features_to_use_in_simulation[feature_ind]) 
   } else {
     plot_outcome_set(collated_realisations,
-                     plot_site_offset_outcome = TRUE, 
-                     plot_site_dev_outcome = FALSE, 
+                     plot_site_offset_outcome, 
+                     plot_site_dev_outcome, 
                      output_type,
                      current_policy_params,
                      site_outcome_plot_lims_set[[plot_ind]],
