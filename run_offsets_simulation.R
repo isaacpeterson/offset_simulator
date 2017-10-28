7cd061d360e112aecca35f71f1c6cce8a97619b6
isaacpeterson <isaac.p.mail@gmail.com>
2017-10-28 12:39:17 +0200

    remove testing code

--- run_offsets_simulation.R
+++ run_offsets_simulation.R
@@ -24,16 +24,7 @@ for (scenario_ind in seq_along(run_params$policy_params_group)){
   loop_strt <- Sys.time()
   cat('\nrunning scenario', scenario_ind, 'of', length(run_params$policy_params_group), 'with', run_params$realisation_num, 
                'realisations on', run_params$crs, ' cores \n')
-#   for (realisation_ind in seq_len(run_params$realisation_num)){
-#     simulation_outputs <- run_offset_simulation_routines(policy_params = run_params$policy_params_group[[scenario_ind]], 
-#                                                          run_params,
-#                                                          parcels, 
-#                                                          initial_ecology, 
-#                                                          decline_rates_initial,
-#                                                          dev_weights,
-#                                                          scenario_ind, 
-#                                                          realisation_ind)
-#   }
+
   if (run_params$realisation_num > 1){
     foreach(realisation_ind = seq_len(run_params$realisation_num)) %dopar%{
       
