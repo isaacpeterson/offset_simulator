2204749132f9245a7f9c5712fb31c7621d4c1929
isaacpeterson <isaac.p.mail@gmail.com>
2017-12-03 15:48:22 +0545

    move parallel processing initialisation to initialise routines

--- run_offsets_simulation.R
+++ run_offsets_simulation.R
@@ -1,23 +1,28 @@
 rm(list = ls())
 source('initialise_routines.R')                              # functions to collate simulation outputs
 
-user_params_file = 'user_params/initialise_params_scale_paper.R'
+user_params_file = 'user_params/initialise_params_testing_new.R'
 
 run_params <- run_initialise_routines(user_params_file)
+
+# nested list object containing ecological values by feature layer for all sites
 initial_ecology <- readRDS(paste0(run_params$simulation_inputs_folder, 'parcel_ecology.rds'))
+
+# list containing information on site geography
 parcels <- readRDS(paste0(run_params$simulation_inputs_folder, 'parcels.rds'))
+
+# list containing probabilities of sites being developed
 dev_weights <- readRDS(paste0(run_params$simulation_inputs_folder, 'dev_weights.rds'))
 
+# list used to govern ecology rate changes
 decline_rates_initial <- simulate_decline_rates(parcel_num = length(parcels$land_parcels), 
                                                 sample_decline_rate = TRUE, 
                                                 mean_decline_rates = run_params$mean_decline_rates, 
                                                 decline_rate_std = run_params$decline_rate_std, 
                                                 feature_num = run_params$feature_num)       # set up array of decline rates that are eassociated with each cell
-
+# select subset of ecology to use in current simulation 
+# (e.g. if initial ecology is 100 layers deep just run with 10 of them)
 initial_ecology <- select_feature_subset(initial_ecology, run_params$features_to_use_in_simulation)
-
-cl<-makeCluster(run_params$crs)  # allow parallel processing on n = 4 processors
-registerDoParallel(cl)
 
 for (scenario_ind in seq_along(run_params$policy_params_group)){
   
@@ -26,6 +31,7 @@ for (scenario_ind in seq_along(run_params$policy_params_group)){
                'realisations on', run_params$crs, ' cores \n')
 
   if (run_params$realisation_num > 1){
+    # run simulation realisations in parallel
     foreach(realisation_ind = seq_len(run_params$realisation_num)) %dopar%{
       
       simulation_outputs <- run_offset_simulation_routines(policy_params = run_params$policy_params_group[[scenario_ind]], 
@@ -38,6 +44,7 @@ for (scenario_ind in seq_along(run_params$policy_params_group)){
                                                            realisation_ind)
     }
   } else {
+    #run single simulation realisation
     simulation_outputs <- run_offset_simulation_routines(policy_params = run_params$policy_params_group[[scenario_ind]], 
                                                          run_params,
                                                          parcels, 
@@ -54,8 +61,14 @@ for (scenario_ind in seq_along(run_params$policy_params_group)){
 }
 
 if (run_params$save_simulation_outputs == FALSE){
+  #remove all temporary files
   unlink(run_params$output_folder, recursive = TRUE)
 }
 
 cat('\nall scenarios done in', round(difftime(Sys.time(), run_params$strt), 1), units(difftime(Sys.time(), run_params$strt)))
-stopCluster(cl)
+
+if (run_params$realisation_num > 1){
+  stopCluster(cl)
+}
+
+
