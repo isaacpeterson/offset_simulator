7e8ad10658aef6b25b9fd075b6ee39825f7a67b1
isaacpeterson <isaac.p.mail@gmail.com>
2017-12-19 12:32:14 +1100

    add run_offset_simulation.R to source code

---
+++ R/run_offset_simulation.R
@@ -0,0 +1,14 @@
+rm(list = ls())
+
+library(offsetsim)
+
+source('user_params/initialise_params_testing_new.R')
+
+user_variable_params = initialise_variable_params()
+user_params = initialise_user_params()
+
+osim.run(user_params, user_variable_params, loglevel = 'TRACE')
+
+source('user_params/plot_params.R')
+plot_params <- initialise_plot_params()
+osim.plot(plot_params, user_params, loglevel = 'TRACE')
