5e8deeb041e783440f097999e3c10407163ddb6a
isaacpeterson <isaac.p.mail@gmail.com>
2017-12-19 12:37:01 +1100

    functionaries run_offset_simulation

--- R/run_offset_simulation.R
+++ R/run_offset_simulation.R
@@ -1,12 +1,17 @@
-library(offsetsim)
+run_offset_simulation <- function(simulation_user_params_file, plot_user_params_file){
+  library(offsetsim)
+  
+  source(simulation_user_params_file)
+  
+  user_variable_params = initialise_variable_params()
+  user_params = initialise_user_params()
+  
+  osim.run(user_params, user_variable_params, loglevel = 'TRACE')
+  
+  source(plot_user_params_file)
+  plot_params <- initialise_plot_params()
+  osim.plot(plot_params, user_params, loglevel = 'TRACE')
+}
 
-source('user_params/initialise_params_testing_new.R')
-
-user_variable_params = initialise_variable_params()
-user_params = initialise_user_params()
-
-osim.run(user_params, user_variable_params, loglevel = 'TRACE')
-
-source('user_params/plot_params.R')
-plot_params <- initialise_plot_params()
-osim.plot(plot_params, user_params, loglevel = 'TRACE')
+run_offset_simulation(simulation_user_params_file = 'user_params/initialise_params_testing_new.R', 
+                      plot_user_params_file = 'user_params/plot_params.R')
