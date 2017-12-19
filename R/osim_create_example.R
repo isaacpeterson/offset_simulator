a803cd588f7d7fe803a3f4247849d5d08370b0fc
Dhirendra Singh <dhi.singh@gmail.com>
2017-12-19 15:14:36 +1100

    Adding in code to create user example

---
+++ R/osim_create_example.R
@@ -0,0 +1,53 @@
+#' Creates an offsetsim usage example file 'offsetsim-example.R' in the given directory
+#' @param outdir the output directory to create the user example file in (default is '.')
+#' @param loglevel logging level to use, for instance futile.logger::INFO
+#' @import futile.logger
+#' @import rprojroot
+#' @export
+osim.create.example <- function(outdir = '.', loglevel = INFO) {
+  
+  urlRunParams <- 'https://raw.githubusercontent.com/isaacpeterson/offset_simulator/master/R/default_initialise_params.R'
+  urlPlotParams <- 'https://raw.githubusercontent.com/isaacpeterson/offset_simulator/master/R/default_plot_params.R'
+  urlRunScript <- 'https://raw.githubusercontent.com/isaacpeterson/offset_simulator/master/user_examples/run_offset_simulation_source_code.R'
+
+  flog.threshold(loglevel)
+  flog.info(paste0("Ensuring output directory '", outdir, "' exists"))
+  outfolder <- write_nested_folder(outdir)
+  
+  outRunScript <- paste0(outfolder,'/offsetsim_example.R')
+  outRunParams <- paste0(outfolder,'/default_initialise_params.R')
+  outPlotParams <- paste0(outfolder,'/default_plot_params.R')
+  
+  
+  flog.info(paste('Writing to ', outfolder)) 
+  quiet <- FALSE
+  if (loglevel == futile.logger::ERROR || loglevel == futile.logger::WARN || loglevel == futile.logger::INFO) {
+    quiet <- TRUE
+  }
+  msg <- paste0("Could not download user example fiels to ", outfolder, ".\n",
+                "Please ensure you have an active internet connection.\n")
+  
+  flog.info(paste('Writing ', urlRunScript, 'to', outRunScript)) 
+  tryCatch(download.file(urlRunScript,outRunScript, quiet = quiet),
+           warning = function(err) cat(msg),
+           error = function(err) cat(msg)
+  )
+  
+  flog.info(paste('Writing ', urlRunParams, 'to', outRunParams)) 
+  tryCatch(download.file(urlRunParams,outRunParams, quiet = quiet),
+           warning = function(err) cat(msg),
+           error = function(err) cat(msg)
+  )
+  
+  flog.info(paste('Writing ', urlPlotParams, 'to', outPlotParams)) 
+  tryCatch(download.file(urlPlotParams,outPlotParams, quiet = quiet),
+           warning = function(err) cat(msg),
+           error = function(err) cat(msg)
+  )
+  
+  cat('User example downloaded to folder "', outfolder, 
+      '";". To run the example do:\nsetwd("',outfolder,'"); source("',outRunScript,'")\n'
+      , sep = "")
+}
+
+
