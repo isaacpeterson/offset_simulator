f7cc50548dc2f5d8b2f8cca05d71b535158a6100
isaacpeterson <isaac.p.mail@gmail.com>
2018-03-08 15:07:46 +1100

    set unlink to remove boundary test folder after test has passed

--- tests/testthat/test-user-example.R
+++ tests/testthat/test-user-example.R
@@ -92,7 +92,7 @@ test_that("running user-example", {
     lines(as.numeric(actual_impacts[, plot_ind ]))
   }
   dev.off()
-  unlink(paste0(dirname(run_file_dir), '/output/boundary_test'))
+  unlink(paste0(dirname(run_file_dir), '/output/boundary_test/'), recursive = TRUE)
   #   expect_true(program_impacts > 0)
   #   expected <- md5sum(paste0('../expected/test02out/collated_scenario_001_realisation_001_feature_001.rds'))
   #   actual <- md5sum(paste0('../output/test02out/simulation_runs/00001/collated_outputs/collated_scenario_001_realisation_001_feature_001.rds'))
