e9ee59f2f61f3b732627342b8da42c4d3d683b02
isaacpeterson <isaac.p.mail@gmail.com>
2018-03-12 20:54:44 +1100

    update parameter names

--- R/run_offsets_simulation.R
+++ R/run_offsets_simulation.R
@@ -19,7 +19,7 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
   # simulated data if required or reading in previously generated input data
   params_object <- run_initialise_routines(user_global_params, user_simulation_params, user_simulated_ecology_params)
   
-
+  
   # Read in list object containing feature values by feature layer for all
   # sites.  This is 3-level nested list, the top level is parcel index, for each
   # parcel there is sublist containing the value of each feature for each cell
@@ -28,10 +28,10 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
   # containing the values for the 2 features in that parcel. The values of the
   # feature for the parcel are a vector of length given by the number of
   # pixels in the parcel.
-
-  initial_features <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'parcel_ecology.rds'))
   
-
+  initial_feature_layers <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'parcel_ecology.rds'))
+  
+  
   # This is a list of vectors of length number of sites. First level is the parcel index; the second
   # level is the a vector containing the index values for the pixels in each
   # parcel
@@ -56,7 +56,7 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
   # number represent the rate that each fature changes over time. For now this
   # assumes that there is an analytic expression for the how each feature
   # changes over time.
-
+  
   # TODO(Isaac): for Sydney model, will need to be able to use arbitrary
   # curves given by experts. In that case this number will change to represent
   # something like  the identity fo the lookup table that describes chondition
@@ -69,44 +69,44 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
             length(params_object$simulation_params_group),  
             params_object$global_params$realisation_num,
             params_object$global_params$number_of_cores ) 
-
-
+  
+  
   # Loop over all defined scenarios if only a subset of the scenarios is to be
   # reun (as defined by params_object$global_params$scenario_subset) then only
   # run these. By default params_object$global_params$scenario_subset
-
+  
   # TODO(Issac) change scenario_run_vec to be scenario_subset
-
+  
   for (scenario_ind in params_object$global_params$scenario_subset){
     
     # Store the start time
     loop_strt <- Sys.time()
     
-
+    
     # Extract out the parameters for the current scenario to be run
     current_simulation_params <- params_object$simulation_params_group[[scenario_ind]]
-
+    
     flog.info('running scenario %s of %s in %s mode with %s offsets',  
               scenario_ind, 
               length(params_object$simulation_params_group), 
               current_simulation_params$offset_calc_type, 
               current_simulation_params$offset_action_type)
     
-
+    
     # select subset of feature layers to use in current simulation 
     # (e.g. if there 100 layers just run with 10 of them)
-    initial_features_to_use <- select_feature_subset(initial_features, current_simulation_params$features_to_use_in_simulation)
+    initial_feature_layers_to_use <- select_feature_subset(initial_feature_layers, current_simulation_params$features_to_use_in_simulation)
     
     decline_rates_initial_to_use <- select_feature_subset(decline_rates_initial, current_simulation_params$features_to_use_in_simulation)
     
     # Set up the object used to store all simulation inputs and pass them to the simulation function 
     simulation_inputs = initialise_input_object(parcels, 
-                                                 initial_features_to_use, 
-                                                 current_simulation_params, 
-                                                 decline_rates_initial_to_use, 
-                                                 dev_weights, 
-                                                 offset_weights)
-    
+                                                initial_feature_layers_to_use, 
+                                                current_simulation_params, 
+                                                decline_rates_initial_to_use, 
+                                                offset_weights, 
+                                                dev_weights)
+
     flog.info('developing %s of %s available sites with %s available offset_sites in a landscape with %s sites and %s x %s elements', 
               sum(current_simulation_params$intervention_vec), 
               length(unlist(simulation_inputs$index_object$indexes_to_use$devs)),  # total number of sites available to develop
@@ -117,7 +117,7 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
     
     # Work out if more than one core is specified, and if so, run the
     # simulation in parallel using the doParallel, foreach and doRNG packages
-
+    
     # TODO(Isaac) test whether can set check and set seed if necessary much
     # earlier in the code before run_initialise_routines() is called
     if (params_object$global_params$number_of_cores > 1 && params_object$global_params$set_seed == TRUE){
@@ -125,7 +125,7 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
       # doRNG needed to get deterministic foreach loops, dsingh 24/nov/17
       flog.info('will use doRNG with seed %s to get determinisitc parallel runs', 123)
       registerDoRNG(123) 
-
+      
       # foreach runs realizations in parallel
       foreach(realisation_ind = seq_len(params_object$global_params$realisation_num)) %dorng%{
         
@@ -133,7 +133,6 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
                                        current_simulation_params,
                                        params_object$global_params,
                                        parcels,
-                                       initial_features_to_use,
                                        decline_rates_initial_to_use,
                                        dev_weights,
                                        offset_weights,
@@ -149,7 +148,6 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
                                        current_simulation_params,
                                        params_object$global_params,
                                        parcels,
-                                       initial_features_to_use,
                                        decline_rates_initial_to_use,
                                        dev_weights,
                                        offset_weights,
@@ -164,7 +162,6 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
                                        current_simulation_params,
                                        params_object$global_params,
                                        parcels,
-                                       initial_features_to_use,
                                        decline_rates_initial_to_use,
                                        dev_weights,
                                        offset_weights,
@@ -177,7 +174,6 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
               scenario_ind,
               round(difftime(Sys.time(), loop_strt), 1), 
               units(difftime(Sys.time(), loop_strt)))
-    
   }
   
   if (params_object$global_params$save_simulation_outputs == FALSE){
@@ -191,7 +187,7 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
             units(difftime(Sys.time(), params_object$global_params$strt)))
   
   flog.info('all outputs written into %s', params_object$global_params$run_folder)
-
+  
   # Clean up the parallel processes if there was more than one
   parallel::stopCluster(params_object$global_params$clstr)
 }
