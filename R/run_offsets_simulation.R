e30d34641d6e976cac60b9d4404a988e3ade8cac
isaacpeterson <isaac.p.mail@gmail.com>
2018-04-29 16:22:24 +1000

    move to loading files from within initialise_routines_script and passing input object to simulation

--- R/run_offsets_simulation.R
+++ R/run_offsets_simulation.R
@@ -9,68 +9,14 @@
 #' @import futile.logger
 #' @export
 #' 
-osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, user_simulated_ecology_params = NULL, loglevel = WARN){
-  
-  flog.appender(appender.tee('osim.run.log'))
+osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, user_simulated_ecology_params = NULL, user_simulation_dynamics = NULL, loglevel = WARN){
   
   flog.threshold(loglevel)
   flog.info('starting offsetsim')
   
   # Undertake all the run intialization proceedures, including generating
   # simulated data if required or reading in previously generated input data
-  params_object <- run_initialise_routines(user_global_params, user_simulation_params, user_simulated_ecology_params)
-  
-  
-  # Read in list object containing feature values by feature layer for all
-  # sites.  This is 3-level nested list, the top level is parcel index, for each
-  # parcel there is sublist containing the value of each feature for each cell
-  # in that parcel. So if there were 3 parcels and 2 features, it would be a 3
-  # element list, where each element was subsequent list of length 2
-  # containing the values for the 2 features in that parcel. The values of the
-  # feature for the parcel are a vector of length given by the number of
-  # pixels in the parcel.
-  
-  initial_feature_layers <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'parcel_ecology.rds'))
-  
-  
-  # This is a list of vectors of length number of sites. First level is the parcel index; the second
-  # level is the a vector containing the index values for the pixels in each
-  # parcel
-  parcels <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'parcels.rds'))
-  
-  # This is a list of single values of length number of sites where the values
-  # representing the probabilities of sites being developed. Default is that
-  # all sites have equal probability simulated data, with user data this must
-  # be specified.
-  # TODO(Isaac): if this isn't supplied by user, assign all values to be equal (and give warning)
-  dev_probability_list <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'dev_probability_list.rds'))
-  
-  # This is a list of single values of length number of sites, where the values
-  # represent the probabilities of sites being offset. Default is that all
-  # sites have equal probability simulated data, with user data this must be
-  # specified.
-  # TODO(Isaac): if this isn't supplied by user, assign all values to be equal (and give warning)
-  offset_probability_list <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'offset_probability_list.rds'))
-  
-  # This is a nested list. Top level is of length number of sites, and each
-  # element is another list containing a single number for each feature.  This
-  # number represent the rate that each fature changes over time. For now this
-  # assumes that there is an analytic expression for the how each feature
-  # changes over time.
-  
-  # TODO(Isaac): for Sydney model, will need to be able to use arbitrary
-  # curves given by experts. In that case this number will change to represent
-  # something like  the identity fo the lookup table that describes chondition
-  # change over time, which can be based on a funciton or can be from expert
-  # elicitation
-  
-  #decline_rates_initial <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'decline_rates_initial.rds'))
-  
-  background_dynamics <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'background_dynamics.rds'))
-  
-  management_dynamics <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'management_dynamics.rds'))
-  
-  management_mode <- readRDS(paste0(params_object$global_params$simulation_inputs_folder, 'management_mode.rds'))
+  input_object <- run_initialise_routines(user_global_params, user_simulation_params, user_simulated_ecology_params, user_simulation_dynamics)
   
   # Write initial logging info
   flog.info('Running %s scenarios with %s realisations on %s cores', 
@@ -88,7 +34,13 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
     loop_strt <- Sys.time()
     
     # Extract out the parameters for the current scenario to be run
-    current_simulation_params <- params_object$simulation_params_group[[scenario_ind]]
+    current_simulation_params <- input_object$simulation_params_group[[scenario_ind]]
+    
+    index_object <- initialise_index_object(parcels, 
+                                            initial_feature_layers, 
+                                            current_simulation_params, 
+                                            offset_indexes_to_exclude = c(which(unlist(input_object$offset_probability_list) == 0)), 
+                                            dev_indexes_to_exclude = which(unlist(input_object$dev_probability_list) == 0))
     
     flog.info('running scenario %s of %s in %s mode with %s offsets',  
               scenario_ind, 
@@ -96,22 +48,6 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
               current_simulation_params$offset_calc_type, 
               current_simulation_params$offset_action_type)
     
-    
-    # select subset of feature layers to use in current simulation 
-    # (e.g. if there 100 layers just run with 10 of them)
-    initial_feature_layers_to_use <- select_feature_subset(initial_feature_layers, current_simulation_params$features_to_use_in_simulation)
-    
-    background_dynamics_to_use <- select_feature_subset(background_dynamics, current_simulation_params$features_to_use_in_simulation)
-    
-    management_dynamics_to_use <- select_feature_subset(management_dynamics, current_simulation_params$features_to_use_in_simulation)
-    
-    # Set up the object used to store all simulation inputs and pass them to the simulation function 
-    simulation_inputs = initialise_input_object(parcels, 
-                                                initial_feature_layers_to_use, 
-                                                current_simulation_params, 
-                                                offset_probability_list, 
-                                                dev_probability_list)
-
     flog.info('developing %s of %s available sites with %s available offset_sites in a landscape with %s sites and %s x %s elements', 
               sum(current_simulation_params$intervention_vec), 
               length(unlist(simulation_inputs$index_object$indexes_to_use$devs)),  # total number of sites available to develop
@@ -134,15 +70,9 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
       # foreach runs realizations in parallel
       foreach(realisation_ind = seq_len(params_object$global_params$realisation_num)) %dorng%{
         
-        run_offset_simulation_routines(simulation_inputs, 
+        run_offset_simulation_routines(input_object, 
                                        current_simulation_params,
-                                       params_object$global_params,
-                                       parcels,
-                                       background_dynamics_to_use, 
-                                       management_dynamics_to_use,
-                                       management_mode,
-                                       dev_probability_list,
-                                       offset_probability_list,
+                                       index_object,
                                        scenario_ind,
                                        realisation_ind)
       }
@@ -151,15 +81,9 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
       # case when running NON-DETERMINISTIC realisations in parallel
       foreach(realisation_ind = seq_len(params_object$global_params$realisation_num)) %dopar%{
         
-        run_offset_simulation_routines(simulation_inputs, 
+        run_offset_simulation_routines(input_object, 
                                        current_simulation_params,
-                                       params_object$global_params,
-                                       parcels,
-                                       background_dynamics_to_use, 
-                                       management_dynamics_to_use,
-                                       management_mode,
-                                       dev_probability_list,
-                                       offset_probability_list,
+                                       index_object,
                                        scenario_ind,
                                        realisation_ind)
       }
@@ -167,15 +91,9 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
       # Case when running single realisation
       # TODO(Isaac): need to add case for running a single realization either with or without having the seed set.
       for (realisation_ind in 1:params_object$global_params$realisation_num){
-        run_offset_simulation_routines(simulation_inputs, 
+        run_offset_simulation_routines(input_object,
                                        current_simulation_params,
-                                       params_object$global_params,
-                                       parcels,
-                                       background_dynamics_to_use, 
-                                       management_dynamics_to_use,
-                                       management_mode,
-                                       dev_probability_list,
-                                       offset_probability_list,
+                                       index_object,
                                        scenario_ind,
                                        realisation_ind)
       }
