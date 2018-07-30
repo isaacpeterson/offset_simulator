3f87067405aee9a56199f963313e97ab584b033f
isaacpeterson <isaac.p.mail@gmail.com>
2018-07-31 08:53:10 +1000

    separate and clean up load params

--- R/run_offsets_simulation.R
+++ R/run_offsets_simulation.R
@@ -16,41 +16,41 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
   flog.threshold(loglevel)
   flog.info('starting offsetsim')
   
-  params_object <- build_simulation_params(user_global_params, user_simulation_params, user_feature_params)
+  global_params <- build_global_params(user_global_params)
+  simulation_params_group = build_simulation_params_group(user_simulation_params)
+  feature_params <- build_feature_params(user_feature_params)
   
+  global_params <- write_params_to_disk(global_params, simulation_params_group)
+    
   # Undertake all the run intialization proceedures, including generating
   # simulated data if required or reading in previously generated input data
   
   # Write initial logging info
   flog.info('Running %s scenarios with %s realisations on %s cores', 
-            length(params_object$simulation_params_group),  
-            params_object$global_params$realisation_num,
-            params_object$global_params$number_of_cores ) 
+            length(simulation_params_group),  
+            global_params$realisation_num,
+            global_params$number_of_cores ) 
   
   # Loop over all defined scenarios if only a subset of the scenarios is to be
-  # reun (as defined by params_object$global_params$scenario_subset) then only
-  # run these. By default params_object$global_params$scenario_subset
+  # reun (as defined by global_params$scenario_subset) then only
+  # run these. By default global_params$scenario_subset
   
-  for (scenario_ind in params_object$global_params$scenario_subset){
+  for (scenario_ind in global_params$scenario_subset){
     
     # Store the start time
     loop_strt <- Sys.time()
     
     # Extract out the parameters for the current scenario to be run
-    current_simulation_params <- params_object$simulation_params_group[[scenario_ind]]
+    current_simulation_params <- simulation_params_group[[scenario_ind]]
+    
+    feature_params_to_use <- select_feature_condition_class_bounds(feature_params, current_simulation_params)
     
     #load objects from files etc
-    input_data_object <- build_input_data(params_object, current_simulation_params)
+    input_data_object <- build_input_data(global_params, feature_params, current_simulation_params)
 
-    index_object <- initialise_index_object(input_data_object$site_characteristics_object, 
-                                            input_data_object$site_feature_layers_initial, 
-                                            current_simulation_params,
-                                            offset_indexes_to_exclude = which(unlist(input_data_object$offset_probability_list) == 0), 
-                                            dev_indexes_to_exclude = which(unlist(input_data_object$dev_probability_list) == 0))
-    
     flog.info('running scenario %s of %s, offsetting using %s impact calculation with %s management regime',  
               scenario_ind, 
-              length(params_object$simulation_params_group),
+              length(simulation_params_group),
               current_simulation_params$offset_calc_type, 
               current_simulation_params$offset_action_type)
     
@@ -58,8 +58,8 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
               input_data_object$site_characteristics_object$landscape_dims[1], 
               input_data_object$site_characteristics_object$landscape_dims[2],
               length(input_data_object$site_characteristics_object$land_parcels),
-              length(index_object$available_indexes$devs), 
-              length(index_object$available_indexes$offsets)) 
+              length(input_data_object$index_object$available_indexes$devs), 
+              length(input_data_object$index_object$available_indexes$offsets)) 
     
     flog.info('developing (and offsetting) %s sites', sum(current_simulation_params$intervention_vec)) 
     
@@ -67,52 +67,47 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
               round(length(input_data_object$site_characteristics_object$land_parcels) -
                 length(input_data_object$site_characteristics_object$land_parcels)*(1 - current_simulation_params$unregulated_loss_prob)^current_simulation_params$time_steps))
     
-    
-    
     # Work out if more than one core is specified, and if so, run the
     # simulation in parallel using the doParallel, foreach and doRNG packages
 
     # TODO(Isaac) test whether can set check and set seed if necessary much
     # earlier in the code before run_initialise_routines() is called
-    if (params_object$global_params$number_of_cores > 1 && params_object$global_params$set_seed == TRUE){
+    if (global_params$number_of_cores > 1 && global_params$set_seed == TRUE){
       # case when running DETERMINISTIC realisations in parallel
       # doRNG needed to get deterministic foreach loops, dsingh 24/nov/17
       flog.info('will use doRNG with seed %s to get determinisitc parallel runs', 123)
       registerDoRNG(123) 
       
       # foreach runs realizations in parallel
-      foreach(realisation_ind = seq_len(params_object$global_params$realisation_num)) %dorng%{
+      foreach(realisation_ind = seq_len(global_params$realisation_num)) %dorng%{
         
         run_offset_simulation_routines(input_data_object, 
                                        current_simulation_params,
-                                       params_object$global_params, 
-                                       params_object$feature_params,
-                                       index_object,
+                                       global_params, 
+                                       feature_params_to_use,
                                        scenario_ind,
                                        realisation_ind)
       }
-    } else if ((params_object$global_params$number_of_cores > 1) && (params_object$global_params$realisation_num > 1)){
+    } else if ((global_params$number_of_cores > 1) && (global_params$realisation_num > 1)){
       # case when running NON-DETERMINISTIC realisations in parallel
-      foreach(realisation_ind = seq_len(params_object$global_params$realisation_num)) %dopar%{
+      foreach(realisation_ind = seq_len(global_params$realisation_num)) %dopar%{
         
         run_offset_simulation_routines(input_data_object, 
                                        current_simulation_params,
-                                       params_object$global_params, 
-                                       params_object$feature_params,
-                                       index_object,
+                                       global_params, 
+                                       feature_params_to_use,
                                        scenario_ind,
                                        realisation_ind)
       }
     } else {
       # Case when running single realisation
       # TODO(Isaac): need to add case for running a single realization either with or without having the seed set.
-      for (realisation_ind in 1:params_object$global_params$realisation_num){
+      for (realisation_ind in 1:global_params$realisation_num){
         
         run_offset_simulation_routines(input_data_object,
                                        current_simulation_params,
-                                       params_object$global_params, 
-                                       params_object$feature_params,
-                                       index_object,
+                                       global_params, 
+                                       feature_params_to_use,
                                        scenario_ind,
                                        realisation_ind)
       }
@@ -124,20 +119,20 @@ osim.run <- function(user_global_params = NULL, user_simulation_params = NULL, u
               units(difftime(Sys.time(), loop_strt)))
   }
   
-  if (params_object$global_params$save_simulation_outputs == FALSE){
+  if (global_params$save_simulation_outputs == FALSE){
     # This deletes all folders and subfolders that were created in the run
     # process that are no longer needed
-    unlink(params_object$global_params$output_folder, recursive = TRUE)
+    unlink(global_params$output_folder, recursive = TRUE)
   }
   
   flog.info('all scenarios done in %s %s', 
-            round(difftime(Sys.time(), params_object$global_params$strt), 1), 
-            units(difftime(Sys.time(), params_object$global_params$strt)))
+            round(difftime(Sys.time(), global_params$strt), 1), 
+            units(difftime(Sys.time(), global_params$strt)))
   
-  flog.info('all outputs written into %s', params_object$global_params$run_folder)
+  flog.info('all outputs written into %s', global_params$run_folder)
   
   # Clean up the parallel processes if there was more than one
-  parallel::stopCluster(params_object$global_params$clstr)
+  parallel::stopCluster(global_params$clstr)
 }
 
 
